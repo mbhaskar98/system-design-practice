ac5b55e6ba7cc8fbd20eb4fb05b59081ffe2c256
bhaskarmahajan <bhaskarmahajan@Bhaskars-MacBook-Pro.local>
2023-09-04 01:38:53 +0530

    [auth] add auth header utils

---
+++ auth/src/main/java/org/bhaskar/utils/http/headerutils/AuthHeaderUtils.java
@@ -0,0 +1,84 @@
+package org.bhaskar.utils.http.headerutils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+
+@Slf4j
+@Component
+public record AuthHeaderUtils() {
+
+    private static final String basicAuthPrefix = "Basic ";
+
+    private @NotNull String extractAuthParams(@NotNull AuthScheme authScheme,
+                                              @NotNull String authorizationHeader) {
+
+        String expectedPrefix = "";
+
+        switch (authScheme) {
+            case BASIC -> {
+                expectedPrefix = AuthHeaderUtils.basicAuthPrefix;
+            }
+            case BEARER -> {
+                log.debug("Bearer");
+                log.error("Not implement:" + authScheme.name());
+            }
+            case DIGEST -> {
+                log.debug("Digest");
+                log.error("Not implement:" + authScheme.name());
+            }
+            default -> log.error("Not implement:" + authScheme.name());
+        }
+
+        if (authorizationHeader.length() < expectedPrefix.length()) {
+            log.error("Header length is less than prefix length:" + expectedPrefix);
+            return "";
+        }
+
+        String actualPrefix = authorizationHeader.substring(0, expectedPrefix.length());
+        log.debug("Header prefix:" + actualPrefix);
+
+
+        if (!actualPrefix.equals(expectedPrefix)) {
+            log.error("Actual prefix:" + actualPrefix + " not equals expected prefix:" + expectedPrefix);
+            return "";
+        }
+
+        return authorizationHeader.substring(expectedPrefix.length());
+    }
+
+    @Contract("_ -> new")
+    public @NotNull BasicAuthCredentials extractBasicAuthUserCredentials(@NotNull String authorizationHeader) {
+
+        String base64Credentials = extractAuthParams(AuthScheme.BASIC, authorizationHeader);
+
+
+        try {
+            // Decode the base64 string
+            byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+            String decodedCredentials = new String(decodedBytes);
+
+            // Split email and password
+            String[] credentials = decodedCredentials.split(":");
+            if (credentials.length == 2) {
+                return new BasicAuthCredentials(credentials[0], credentials[1]);
+            }
+        } catch (IllegalArgumentException e) {
+            log.error("Error while decoding:{}, exception:{}", authorizationHeader, e.getMessage());
+        }
+        return new BasicAuthCredentials("", "");
+    }
+
+    private enum AuthScheme {
+        BASIC,
+        DIGEST,
+        BEARER,
+    }
+
+    public record BasicAuthCredentials(String email,
+                                       String password) {
+    }
+}
---
+++ auth/src/test/java/org/bhaskar/utils/http/headerutils/AuthHeaderUtilsTest.java
@@ -0,0 +1,70 @@
+package org.bhaskar.utils.http.headerutils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+
+class AuthHeaderUtilsTest {
+
+    private AuthHeaderUtils authHeaderUtils;
+    private AuthHeaderUtils.BasicAuthCredentials expected;
+    private AuthHeaderUtils.BasicAuthCredentials actual;
+
+
+    @BeforeEach
+    void setup() {
+        authHeaderUtils = new AuthHeaderUtils();
+    }
+
+    @Nested
+    class BasicAuthHeaderTests {
+
+        @Nested
+        class NegativeTests {
+            @BeforeEach
+            void setupEmpty() {
+                expected = new AuthHeaderUtils.BasicAuthCredentials("", "");
+            }
+
+            @Test
+            @DisplayName("Empty auth header")
+            void emptyHeader() {
+                actual = authHeaderUtils.extractBasicAuthUserCredentials("");
+                Assertions.assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            @DisplayName("Some other scheme than basic")
+            void differentAuthScheme() {
+                actual = authHeaderUtils.extractBasicAuthUserCredentials("abcd asjfnsajf");
+                Assertions.assertThat(actual).isEqualTo(expected);
+            }
+
+            @Test
+            @DisplayName("Some non base 64 encoded")
+            void notBase64Encoded() {
+                actual = authHeaderUtils.extractBasicAuthUserCredentials("Basic ,,asdasd");
+                Assertions.assertThat(actual).isEqualTo(expected);
+            }
+        }
+
+        @Nested
+        class PositiveTests {
+            @BeforeEach
+            void setupEmpty() {
+                expected = new AuthHeaderUtils.BasicAuthCredentials("email", "password");
+            }
+
+            @Test
+            @DisplayName("Basic 64 encoded user name and password")
+            void base64EncodedBasicAuthHeader() {
+                actual = authHeaderUtils.extractBasicAuthUserCredentials("Basic ZW1haWw6cGFzc3dvcmQ=");
+                Assertions.assertThat(actual).isEqualTo(expected);
+            }
+        }
+    }
+
+}
