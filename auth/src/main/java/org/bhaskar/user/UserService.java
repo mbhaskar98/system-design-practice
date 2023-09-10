55f912da2050986b35745fd3003b41e2ee476e66
bhaskarmahajan <bhaskarmahajan98@gmail.com>
2023-09-11 01:08:13 +0530

    [auth] converted extra record classes to normal classes

--- auth/src/main/java/org/bhaskar/user/UserService.java
+++ auth/src/main/java/org/bhaskar/user/UserService.java
@@ -1,10 +1,13 @@
 package org.bhaskar.user;
 
 import org.bhaskar.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public record UserService(UserRepository userRepository) {
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
