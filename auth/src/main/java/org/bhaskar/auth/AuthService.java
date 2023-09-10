c0493cd50bed03d84e140d99fb9ca360e39d4bac
bhaskarmahajan <bhaskarmahajan98@gmail.com>
2023-09-11 00:45:21 +0530

    [auth] auth service added

---
+++ auth/src/main/java/org/bhaskar/auth/AuthService.java
@@ -0,0 +1,28 @@
+package org.bhaskar.auth;
+
+import org.bhaskar.dao.User;
+import org.bhaskar.dto.LoginResponse;
+import org.bhaskar.utils.tokenutils.JWTUtils;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    /**
+     * Generate {@code LoginResponse} object for {@code user}
+     *
+     * @param user current {@code User}
+     * @return {@code LoginResponse}
+     * @throws IllegalArgumentException in case null user is passed
+     */
+    public @NotNull LoginResponse generateJWTResponseForUser(@NotNull User user,
+                                                             @NotNull String secret) throws IllegalArgumentException {
+        String jwtToken = jwtUtils.createJWT(user.getEmail(), secret, user.isAdmin());
+        return new LoginResponse(jwtToken);
+    }
+}
---
+++ auth/src/test/java/org/bhaskar/auth/AuthServiceTest.java
@@ -0,0 +1,75 @@
+package org.bhaskar.auth;
+
+import org.assertj.core.api.Assertions;
+import org.bhaskar.dao.User;
+import org.bhaskar.dto.LoginResponse;
+import org.bhaskar.utils.tokenutils.JWTUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AuthServiceTest {
+
+    private final String secret;
+    private final User user;
+
+    @InjectMocks
+    private AuthService authService;
+    @Mock
+    private JWTUtils jwtUtils;
+
+    AuthServiceTest() {
+        secret = "prod-secret";
+        user = new User(1, "email", "password", false);
+    }
+
+
+    @Nested
+    class GenerateJWTTests {
+
+        @Nested
+        class NegativeTests {
+
+            @Test
+            @DisplayName("Null user")
+            void nullUser() {
+                Assertions.assertThatThrownBy(() -> {
+                    authService.generateJWTResponseForUser(null, secret);
+                }).isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @Test
+            @DisplayName("Null Secret")
+            void nullSecret() {
+
+                Assertions.assertThatThrownBy(() -> {
+                    authService.generateJWTResponseForUser(user, null);
+                }).isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @Nested
+        class PositiveTests {
+
+            @Test
+            @DisplayName("success response")
+            void success() {
+                String token = "token-----asfag";
+                LoginResponse expectedResponse = new LoginResponse(token);
+                Mockito.when(jwtUtils.createJWT(user.getEmail(), secret, user.isAdmin()))
+                        .thenReturn(token);
+
+                Assertions.assertThat(authService.generateJWTResponseForUser(user, secret))
+                        .isEqualTo(expectedResponse);
+            }
+
+        }
+    }
+
+}
